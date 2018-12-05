8af9ebdb52cbbc17ff5de80c17cfa95b1c1e7505
1485535675github <1485535675@qq.com>
2018-12-05 11:48:54 +0800

    1.更改启动类

--- src/main/java/com/SpringmybatisApplication.java
+++ src/main/java/com/SpringmybatisApplication.java
@@ -9,9 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringmybatisApplication  {
 
     public static void main(String[] args){
-
+        System.out.println("-----------------");
         SpringApplication.run(SpringmybatisApplication.class, args);
-
-
     }
 }
