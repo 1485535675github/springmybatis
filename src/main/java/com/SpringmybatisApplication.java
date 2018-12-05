5836bf6f203cf403be1103491054180989e2c9b1
1485535675github <1485535675@qq.com>
2018-12-05 10:58:55 +0800

    1.更改启动类

--- src/main/java/com/SpringmybatisApplication.java
+++ src/main/java/com/SpringmybatisApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan("com.mapper")
 @SpringBootApplication
 public class SpringmybatisApplication  {
+
     public static void main(String[] args){
+
         SpringApplication.run(SpringmybatisApplication.class, args);
     }
 }
